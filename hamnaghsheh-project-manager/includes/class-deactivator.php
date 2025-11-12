0bc587b6bc2b14a20e4038a573077f9d5ff64e1b
soroushyasini <soroush.yasini@outlook.com>
2025-11-12 10:32:37 +0330

    remove outsourced code from codabase

--- hamnaghsheh-project-manager/includes/class-deactivator.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-class Hamnaghsheh_Deactivator {
-    public static function deactivate() {
-        // On deactivate we do not remove data; uninstall.php handles permanent deletion if user removes plugin.
-    }
-}
